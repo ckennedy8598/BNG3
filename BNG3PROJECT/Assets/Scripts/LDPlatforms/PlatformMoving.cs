5274a82f950633a12cbf84a9bf61dad13da26403
ckennedy8598 <156461463+ckennedy8598@users.noreply.github.com>
2024-12-04 15:36:20 -0500

    Insert Creative Title Here
    - pushing progress in case of _
    - fixed the code for the jitter on elevator
    - made script for moving platforms that always move
    - both of my platform types work well now lets go
    - progress on parkour section
    - many new containers and organization
    - misc
    - still working on more ffp reqs but this is a chunk I wanted to push

---
+++ BNG3PROJECT/Assets/Scripts/LDPlatforms/PlatformMoving.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//youtu.be/cW-5JYZLlvQ?si=PSUjY8CERavFwhxJ
+//the vertically moving elevators that i first made use this script as well as TriggerPlatform & ParentPlatform
+
+//this was here by default
+namespace Platformer
+{
+    public class PlatformMoving : MonoBehaviour
+    {
+        public bool canMove;
+
+        [SerializeField] float speed;
+        [SerializeField] int startPoint;
+        [SerializeField] Transform[] points;
+
+        int i;
+        bool reverse;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            transform.position = points[startPoint].position;
+            i = startPoint;
+
+        
+        }
+
+        // Update is called once per frame
+       // changing this from void Update() to void FixedUpdate() fixes the jutter
+        void FixedUpdate()
+        {
+            if (Vector3.Distance(transform.position, points[i].position) < 0.01f)
+            {
+                canMove = false;
+
+                if(i == points.Length - 1)
+                {
+                    reverse = true;
+                    i--;
+                    return;
+                    
+                } else if (i == 0)
+                {
+                    reverse = false;
+                    i++;
+                    return;
+                }
+
+                if (reverse)
+                {
+                    i--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            if (canMove)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
+            }
+        
+        }
+    }
+}
---
+++ BNG3PROJECT/Assets/Scripts/LDPlatforms/TriggerPlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//youtu.be/cW-5JYZLlvQ?si=PSUjY8CERavFwhxJ
+namespace Platformer
+{
+    public class TriggerPlatform : MonoBehaviour
+    {
+
+        PlatformMoving platform;
+
+        private void Start ()
+        {
+            platform = GetComponent<PlatformMoving>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            platform.canMove = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            platform.canMove = true;
+        }
+
+
+
+    }
+}
