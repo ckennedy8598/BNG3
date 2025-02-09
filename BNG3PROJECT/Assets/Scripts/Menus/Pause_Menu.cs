a01e09bf1cd996700aca4d6b4c369cbb79cb31f9
combine1201 <steamuser27@gmail.com>
2025-02-09 15:44:22 -0500

    Small Changes to UI - Nothing notable.

--- BNG3PROJECT/Assets/Scripts/Menus/Pause_Menu.cs
+++ BNG3PROJECT/Assets/Scripts/Menus/Pause_Menu.cs
@@ -106,7 +106,7 @@ public class Pause_Menu : MonoBehaviour
     private void _deathUI_Active()
     {
         Crosshair.SetActive(false);
-        P_MM.SetActive(true); P_Quit.SetActive(true);
+        P_MM.SetActive(true); P_Quit.SetActive(true); P_Retry.SetActive(true);
     }
     
     private void _pauseUI_Active()
