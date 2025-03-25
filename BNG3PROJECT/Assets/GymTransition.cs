2b670790314c02bcdc03ccc6605ee726546e4f2c
Jahpacho <158857348+Jahpacho@users.noreply.github.com>
2025-03-25 15:57:01 -0400

    Maybe the fix?
    Should fix a bug that causes the player to spawn in the incorrect position in the Boss arena.

--- BNG3PROJECT/Assets/GymTransition.cs
+++ BNG3PROJECT/Assets/GymTransition.cs
@@ -7,10 +7,18 @@ namespace Platformer
 {
     public class GymTransition : MonoBehaviour
     {
+        public GameMaster gm;
+
+        private void Awake()
+        {
+            //gm = gameObject.GetComponent<GameMaster>();
+        }
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.name == "PlayerBody")
             {
+                // 9.74  1.69  2.33
+                gm.LastCheckpointPOS = new Vector3(10, 2, 2);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
